fe0a80d010878fe63f3fcd49c189fec52526e7e1
Tadek Chávez <tadek.chavez@gmail.com>
2017-04-11 05:02:12 +1000

    Separation of blocks for displaying and looping through tweets independently

--- resources/views/htcount.blade.php
+++ resources/views/htcount.blade.php
@@ -1,39 +1,62 @@
 @extends('layouts.app')
 @section('content')
 
+<!-- Form wrapper -->
 <div class="full-width" id="usr-input-container">
-	<span class="input input--hoshi">
-		<input type="text" name="screen_name" id="screen_name" class="input__field input__field--hoshi" pattern="[A-Za-z0-9_]+" />
-		<label class="input__label input__label--hoshi input__label--hoshi-color-1" for="screen_name">
-			<span class="input__label-content input__label-content--hoshi">Twitter Username</span>
-		</label>
-	</span>
+	<form action="{{ action('Tweets@fetchTweets') }}" method="POST">
+    	{{ csrf_field() }}
+		<span class="input input--hoshi">
+			<input type="text" name="screen_name" id="screen_name" class="input__field input__field--hoshi" value="{{ $screen_name }}" pattern="[A-Za-z0-9_]+" />
+			<label class="input__label input__label--hoshi input__label--hoshi-color-1" for="screen_name">
+				<span class="input__label-content input__label-content--hoshi">Twitter Username</span>
+			</label>
+		</span>
+		<input type="hidden" value="10" name="num_tweets" />
+		<input type="submit" style="position: absolute; left: -9999px"/>
+	</form>
+
 </div>
+<!--end Form Wrapper -->
 
 <div id="ht" class="flex-container">
+	<!--Tweets Container-->
 	<div class="half-width" id="user-tweets-container">
-		<h2>Last {{ $countTweets }} tweets</h2>
+		<h2>
+			@if ( $countTweets === 1 )
+				Last tweet
+			@elseif( $countTweets > 0 )
+				Last {{ $countTweets }} tweets
+			@else
+				Results
+			@endif
+		</h2>
 	@foreach ($tweets as $tweet)
 		@include( 'partials.tweet', array('tweet', $tweet))
-		
 	@endforeach
 	</div>
+	<!-- end Tweets Container-->
 
+	<!-- Hashtags Container -->
 	<div class="half-width" id="ht-count-container">
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.		
+		@if( $screen_name == '' )
+			<p>
+				Just type the username and hit ENTER to see up to {{ $numTweets}} of the most recent tweets and its hashtags ocurrences.
+			</p>
+		@else
+			<h2>Hashtag Count</h2>
+			<div id="hashtags-wrapper">
+			@foreach ($hashtags as $key => $ht)
+				<div class="ht-item">
+					<a href="https://twitter.com/search?q=%23{{$key}}" target="_blank">
+						{{ $key }} 
+					</a>
+					( {{ $ht }} )
+				</div>
+			@endforeach
+			</div>
+		@endif
 	</div>
+	<!-- End Hashtags Container -->
 </div>
-
-
-<div class="container">
-
-<div ng-view></div>
-
-</div> <!-- /container -->
 
 @endsection
--- resources/views/partials/tweet.blade.php
+++ resources/views/partials/tweet.blade.php
@@ -1,6 +1,6 @@
 <div class="tweet-container">
 	<div class="tweet-description">
-		{{$tweet->text}}
+		{!! $tweet->text !!}
 	</div>
 	<div class="tweet-date">
 		{{ date("h:i a - d M Y", strtotime($tweet->created_at))}}
